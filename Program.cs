7711b94e40b88d89dbbf846d8ed76dca137927b6
Jesús Alejandro Flores Hernández <jflorespampano@gmail.com>
2023-09-08 11:10:56 -0600

    iniciando

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace leer_calif_calc_promedio
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("programa que lee 2 calificaciones, calcula promedio, indica si el alumno paso o no!");
+			
+			string s="";
+			Console.Write("De la calificacion de parcial 1: ");
+			s=Console.ReadLine();
+			int calif1=int.Parse(s);
+			Console.Write("De la calificacion de parcial 2: ");
+			s=Console.ReadLine();
+			int calif2=int.Parse(s);
+			int promedio=(calif1+calif2)/2;
+			if (promedio>=70){
+				Console.WriteLine("aprueba con {0}",promedio);
+			}else{
+			Console.WriteLine("reprueba con {0}",promedio);
+			}
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
