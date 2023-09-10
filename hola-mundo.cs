7cadea3ce7fe7f30cf72b9b83d70701e7613c180
jflorespampano <jflorespampano@gmail.com>
2023-09-10 07:51:49 -0600

    introduccion cadenas

---
+++ hola-mundo.cs
@@ -0,0 +1,13 @@
+using System;
+namespace holamundo{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Your program starts here...
+            Console.WriteLine("Hello world!");
+            Console.ReadKey();
+        }
+    }
+}
+
